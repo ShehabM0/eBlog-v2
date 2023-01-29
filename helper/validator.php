f4aa4f904ec2c50ccce8ba5c61761a6c74bb067e
baher shehab <shehabmohamed2357@gmail.com>
2023-01-29 20:24:31 +0200

    added validator

---
+++ helper/validator.php
@@ -0,0 +1,33 @@
+<?php
+
+function checkForm($array, $keys) {
+    foreach($keys as $key) 
+        if(!isset($array["$key"]) || empty($array["$key"]))
+            return false;
+    return true;
+}
+
+function checkLength($value, $max_length, $min_length = 3) {
+    return strlen($value) <= $max_length && strlen($value) >= $min_length;
+}
+
+function checkImg($image) {
+    $allowed_types = ['jpg', 'png', 'jpeg', 'gif'];
+    foreach($allowed_types as $val)
+        if(str_contains($image, $val))
+            return true;
+    return false;
+}
+
+// testing function
+function lastPostID($id) {
+    global $conn;
+    $query = "SELECT id FROM posts ORDER BY id DESC LIMIT 1";
+    $result = $conn->query($query);
+    if($result)
+        if($id <= 0 || $id > $result->fetch_assoc()['id'])
+            return false;
+    return true;
+}
+
+?>
