840d31f7b49cef071c1367b13cd0f7585d2d76dd
baher shehab <shehabmohamed2357@gmail.com>
2023-01-31 17:28:31 +0200

    myposts page

--- pages/posts/myposts.php
+++ pages/posts/myposts.php
@@ -1,19 +1,10 @@
 <?php
     require_once "../../shared/header.php";
+    require "../../database/post.php";
     if(!isset($_SESSION))
         session_start();
-
-    // if current user is a visitor (not logged-in)
-    // then obviously he doens't have any posts.
-    if(!isset($_SESSION["current_user"]))
-    {
-        header("Error404 Not Found", true, 404);
-        header("location: /blog/404.php");
-    }
-
-    $posts = $_SESSION["posts"];
-    $user_id = $_SESSION["current_user"]["id"];
-    $no_posts = true;
+    $user_id = $_SESSION["user"]["id"];
+    $posts = getAllUserPosts($user_id);
 ?>
 
         <!-- loading user posts -->
@@ -21,26 +12,22 @@
         <?php
             foreach($posts as $post) 
             {
-                if($user_id === $post["user_id"])
-                {
-                    $no_posts = false;
         ?>
                     <a href="/blog/pages/posts/post.php?id=<?= $post["id"] ?>" id="post-link">
                         <div class="mypost">
-                            <img src="<?="/blog/assets/".$post["img"]?>" alt="">
+                            <img src="<?="/blog/uploads/".$post["img"]?>" alt="">
                             <h3><?= $post["title"] ?></h3>
                             <p><?= $post["body"] ?></p>
                             <div class="post-user">
                                 <img src="/blog/assets/bx-user-circle.svg" alt="">
-                                <p><?= $_SESSION["current_user"]["username"] ?></p>
+                                <p><?= $post["username"] ?></p>
                             </div>
                         </div>
                     </a>
-            <?php } ?>
         <?php } ?>
 
         <!-- user has no posts -->
-        <?php if($no_posts) { ?>
+        <?php if(count($posts) == 0) { ?>
             <p id="no-post">You don't have any posts yet.</p>
         <?php } ?>
             
