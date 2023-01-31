1c3367bc2fffb58c7e6505ea836c2062844d5c0b
baher shehab <shehabmohamed2357@gmail.com>
2023-01-31 17:31:07 +0200

    edited myposts

--- pages/posts/myposts.php
+++ pages/posts/myposts.php
@@ -13,17 +13,17 @@
             foreach($posts as $post) 
             {
         ?>
-                    <a href="/blog/pages/posts/post.php?id=<?= $post["id"] ?>" id="post-link">
-                        <div class="mypost">
-                            <img src="<?="/blog/uploads/".$post["img"]?>" alt="">
-                            <h3><?= $post["title"] ?></h3>
-                            <p><?= $post["body"] ?></p>
-                            <div class="post-user">
-                                <img src="/blog/assets/bx-user-circle.svg" alt="">
-                                <p><?= $post["username"] ?></p>
-                            </div>
+                <a href="/blog/pages/posts/post.php?id=<?= $post["id"] ?>" id="post-link">
+                    <div class="mypost">
+                        <img src="<?="/blog/uploads/".$post["img"]?>" alt="">
+                        <h3><?= $post["title"] ?></h3>
+                        <p><?= $post["body"] ?></p>
+                        <div class="post-user">
+                            <img src="/blog/assets/bx-user-circle.svg" alt="">
+                            <p><?= $post["username"] ?></p>
                         </div>
-                    </a>
+                    </div>
+                </a>
         <?php } ?>
 
         <!-- user has no posts -->
